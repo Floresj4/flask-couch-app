f40b9014bb56f9c1b314c6b8ac452f7e24d60861
Jason <floresj4@gmail.com>
2019-05-27 23:03:12 -0400

    creating cookie endpoint

--- service.py
+++ service.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 app = Flask(__name__)
 
 
@@ -9,3 +9,20 @@ def hello():
 @app.route('/hello/<username>', methods = ['GET'])
 def hello_user(username: str):
     return render_template('hello.html', name = username)
+
+@app.route('/cookie/<username>', methods = ['GET'])
+def cookie_user(username: str):
+
+    cookie_username = None
+    try: cookie_username = request.cookies.get('username')
+    except:
+        print('No cookie found.')
+
+    resp = make_response(render_template('hello.html', 
+        name = username, 
+        changed = (cookie_username is not None and cookie_username == username),
+        returning = (cookie_username is not None)))
+
+    resp.set_cookie('username', username)
+    
+    return resp
