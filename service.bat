1509ba94d91912b52373b0afd9d2d02cdf784b1d
Jason <floresj4@gmail.com>
2019-05-31 23:44:08 -0400

    starting couch build

--- service.bat
+++ service.bat
@@ -1,25 +1,31 @@
 @echo off
 
-IF "%1"=="docker-build" GOTO docker-build
-IF "%1"=="docker-run" GOTO docker-run
+IF "%1"=="couch-build" GOTO couch-build
+IF "%1"=="service-build" GOTO docker-build
+IF "%1"=="service-run" GOTO docker-run
 IF "%1"=="local" GOTO local
 GOTO error
+
+:couch-build
+rem build and tag the latest couchdb image
+docker build -f ./_datastore/Dockerfile -t couchdb-service:latest .
+GOTO finish
+
+:service-build
+rem build and tag the latest service image
+docker build -f ./_service/Dockerfile -t flask-app:latest .
+GOTO finish
+
+:service-run
+rem start the container and ensure it's removable on exit
+docker run -it -p 5000:5000 --rm flask-app:latest
+GOTO finish
 
 :local
 rem run the flask application locally
 set FLASK_APP=service.py
 set FLASK_ENV=development
 flask run --host=0.0.0.0
-GOTO finish
-
-:docker-build
-rem build and tag the latest Docker image
-docker build -f ./_service/Dockerfile -t flask-app:latest .
-GOTO finish
-
-:docker-run
-rem start the container and ensure it's removable on exit
-docker run -it -p 5000:5000 --rm flask-app:latest
 GOTO finish
 
 :error
