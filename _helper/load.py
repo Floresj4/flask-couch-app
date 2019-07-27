fd3db22a776edc346ddedf5eb5b081698255b526
Jason <floresj4@gmail.com>
2019-07-27 13:29:09 -0400

    updating logging

--- _helper/load.py
+++ _helper/load.py
@@ -4,7 +4,7 @@ import time
 import os
 
 logging.basicConfig(
-    level = logging.DEBUG,
+    level = logging.INFO,
     handlers = [logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 
@@ -25,22 +25,26 @@ if __name__ == "__main__":
     #sleep a bit to allow the couch instance to come online
     time.sleep(5)
 
-    # load data to encode and post via requests
-    data_json = './data/players.json'
-    print('loading data file {}'.format(data_json))
-    with open(data_json, encoding = 'utf-8') as r:
-        players = json.load(r)
+    try:
 
-    url = 'http://couch:5984/baseball?n=1'
-    logger.debug('Executing PUT request: {}'.format(url))
-    response = requests.put(url, headers = headers)
-    if response.status_code not in [201, 412]:
-        raise Exception("Database creation failed! {}".format(response.text))
+        # load data to encode and post via requests
+        data_json = './data/players.json'
+        print('loading data file {}'.format(data_json))
+        with open(data_json, encoding = 'utf-8') as r:
+            players = json.load(r)
+
+        url = 'http://couch:5984/baseball?n=1'
+        response = requests.put(url, headers = headers)
+        if response.status_code not in [201, 412]:
+            raise Exception("Database creation failed! {}".format(response.text))
+        logger.info('PUT request completed successfully: {}'.format(url))
+
+        url = 'http://couch:5984/baseball/_bulk_docs'
+        response = requests.post(url, headers = headers, data = json.dumps(players))
+        if(response.status_code != 201):
+            raise Exception("Loading team data failed! {}".format(response.text))
+        logger.info('POST request completed successfully: {}'.format(url))
+        logger.info('Database loading completed successfully.')
     
-    url = 'http://couch:5984/baseball/_bulk_docs'
-    logger.debug('Executing PUT request: {}'.format(url))
-    response = requests.post(url, headers = headers, data = json.dumps(players))
-    if(response.status_code != 201):
-        raise Exception("Loading team data failed! {}".format(response.text))
-    
-    logger.debug('Database loading completed successfully.')
+    except Exception as e:
+        logger.error("An error occurred during database setup. {}".format(e))
