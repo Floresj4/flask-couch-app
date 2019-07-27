ad469c2c784556be8ef693e27c801bcf287aadec
Jason <floresj4@gmail.com>
2019-07-27 00:20:07 -0400

    loading db from python

--- _helper/load.py
+++ _helper/load.py
@@ -1,20 +1,27 @@
 import requests, json
 import logging
+import time
 import os
 
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
 
-    print('hey...')
+    #sleep a bit to allow the couch instance to come online
+    time.sleep(5)
 
+    # load data to encode and post via requests
+    data_json = './data/players.json'
+    print('loading data file {}'.format(data_json))
+    with open(data_json, encoding = 'utf-8') as r:
+        players = json.load(r)
 
-    print(os.listdir("."))
+    headers = {
+        "content-type": "application/json"
+    }
 
-    #load data to encode and post via requests
-    # data_json = './data/players.json'
-    # logger.info('loading data file {}'.format(data_json))
-    # open with(data_json, 'r') as r:
-    #     players = json.load(r)
-
-    # requests.post()
+    response = requests.put('http://couch:5984/baseball?n=1', headers = headers)
+    print(response.status_code)
+    response = requests.post('http://couch:5984/baseball/_bulk_docs', headers = headers, data = json.dumps(players))
+    print(response.status_code)
+    print(response.text)
